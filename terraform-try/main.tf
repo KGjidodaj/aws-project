120a34ab99c388245491031152838d0196f56758
KGjidodaj <kristiangjidodaj@gmail.com>
2026-06-04 02:29:18 +0300

    fix: tested terraform fmt for readability

--- terraform-try/main.tf
+++ terraform-try/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.0" #limiting to major 6.x releases
     }
   }
@@ -15,10 +15,10 @@ provider "aws" {
 resource "aws_vpc" "main_network" {
   cidr_block = "10.0.0.0/16"
   #Enabling DNS access for high availability in case of ip change
-  enable_dns_support = true
+  enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-  Name = "AWS-project-VPC"
+    Name = "AWS-project-VPC"
   }
 }
 
@@ -26,15 +26,15 @@ resource "aws_vpc" "main_network" {
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main_network.id
   tags = {
-  Name = "AWS-project-IGW"
+    Name = "AWS-project-IGW"
   }
 }
 
 #Public Subnet
 resource "aws_subnet" "public_subnet" {
-  vpc_id = aws_vpc.main_network.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "eu-north-1a"
+  vpc_id                  = aws_vpc.main_network.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-north-1a"
   map_public_ip_on_launch = true #Gives all IPs automatically
   tags = {
     Name = "AWS-project-Public-Subnet"
@@ -55,56 +55,56 @@ resource "aws_route_table" "public_rt" {
 
 #Table association with subnet
 resource "aws_route_table_association" "public_rt_assoc" {
-  subnet_id = aws_subnet.public_subnet.id
+  subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
 
 #Security Groups Part:
 #Nginx SG
 resource "aws_security_group" "nginx_bastion_sg" {
-  name = "nginx-bastion-sg"
+  name        = "nginx-bastion-sg"
   description = "Allow inbound HTTP/HTTPS and SSH from anywhere"
-  vpc_id = aws_vpc.main_network.id
+  vpc_id      = aws_vpc.main_network.id
   ingress {
     description = "HTTP from Internet"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "SSH from Admin and CD pipeline"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     description = "Allow all outbound traffic"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"] #using 0.0.0.0/0 for now
   }
 }
 
 #Internal App and DB SG
 resource "aws_security_group" "internal_sg" {
-  name = "internal-app-db-sg"
+  name        = "internal-app-db-sg"
   description = "Allow inbound traddic only from Nginc bastion SG"
-  vpc_id = aws_vpc.main_network.id
+  vpc_id      = aws_vpc.main_network.id
   ingress {
-    description = "Allow all traffic from bastion host"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    description     = "Allow all traffic from bastion host"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
     security_groups = [aws_security_group.nginx_bastion_sg.id]
   }
   egress {
     description = "Allow all outbound traffic for internet access (e.g. OS updates)"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
