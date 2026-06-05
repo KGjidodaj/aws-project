e131fa0fb03b8567073ecfdca4479e1f0436d25c
KGjidodaj <kristiangjidodaj@gmail.com>
2026-06-05 16:59:28 +0300

    chore: testing interactive script to save aws cloud billing with terraform lifecycle

---
+++ terraform-try/money_saver.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#Colours for results
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color for the resets
+
+echo "Running terraform validate to check the code, might take a little time..."
+terraform validate
+
+echo "Running terraform apply to apply any changes, might take a little time..."
+if terraform apply; then
+    echo -e "\n\n\n${GREEN}Terraform apply worked.--------------------${NC}"
+    read -p "Pausing the script before destruction, do whatever you need and then press enter."
+
+    echo "Starting the teardown process with terraform destroy."
+    sleep 0.5
+    if terraform destroy; then
+        echo -e "${GREEN}[+] Destroy command finished, billing stopped.${NC}"
+    else
+        echo -e "${RED}[!] ERROR: Command stopped get now into the aws cloud account and disable manually to stop billing!${NC}"
+        exit 1
+    fi
+
+else
+    echo -e "${RED}[!] ERROR: Terraform apply stopped re run the script and if needed stop the billings via the aws cloud account.${NC}"
+    exit 1
+fi
