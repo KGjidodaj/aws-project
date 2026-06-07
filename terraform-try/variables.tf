2d2ef445cd3e19ea5d14313e7d5e7ad466d80ea9
KGjidodaj <kristiangjidodaj@gmail.com>
2026-06-07 23:59:52 +0300

    fix: resolve OOM starvation and update telemetry tagging

--- terraform-try/variables.tf
+++ terraform-try/variables.tf
@@ -1,7 +1,7 @@
 variable "instance_type" {
   description = "Free tier instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t3.small"
 }
 variable "key_name" {
   description = "Using the AWS key pair linked to the aws-project directory for the EC2 instances"
